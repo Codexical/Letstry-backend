707afcbd379a6e828a2058f8f59592956d551f5e
Aura <aura.cs12@nycu.edu.tw>
2024-05-09 15:57:14 +0800

    create user function

--- server.py
+++ server.py
@@ -97,5 +97,63 @@ def login():
         )
 
 
+@app.route("/create", methods=["POST"])
+def create():
+    """
+    Create a new user
+    ---
+    parameters:
+      - name: username
+        in: formData
+        type: string
+        required: true
+      - name: account
+        in: formData
+        type: string
+        required: true
+      - name: password
+        in: formData
+        type: string
+        required: true
+    responses:
+        200:
+            description: OK
+            examples:
+                application/json: {"status": "ok", "ID": 0, "username": "name"}
+        400:
+            description: User already exists
+            examples:
+                application/json: {"status": "User already exists"}
+    """
+    username = request.values["username"]
+    account = request.values["account"]
+    password = request.values["password"]
+    if account in users:
+        return Response(
+            json.dumps({"status": "User already exists"}),
+            status=400,
+            mimetype="application/json",
+        )
+    else:
+        users[account] = {
+            "ID": len(users),
+            "username": username,
+            "password": password,
+        }
+        with open("storage/users.json", "w") as f:
+            json.dump(users, f, indent=4)
+        return Response(
+            json.dumps(
+                {
+                    "status": "ok",
+                    "ID": users[account]["ID"],
+                    "username": users[account]["username"],
+                }
+            ),
+            status=200,
+            mimetype="application/json",
+        )
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
